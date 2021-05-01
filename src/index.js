b71b750bb3e5169d5eb0aef7675c39c8cea0eba7
Chairenerose <76175773+Chairenerose@users.noreply.github.com>
2021-05-01 20:03:24 +0200

    Mobile responsive

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ const navbarlinks = document.getElementsByClassName('nav-links')[0]
 hamburger.addEventListener('click', () => {
     navbarlinks.classList.toggle('active')
 })
+
+
+const statementbutton = document.getElementsByClassName('commitmentbutton')[0]
+const statement = document.getElementsByClassName('commitment')[0]
+
+statementbutton.addEventListener('click', () => {
+    statement.classList.toggle('active')
+})
