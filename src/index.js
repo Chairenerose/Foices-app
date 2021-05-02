37cedf65951747f32aa4f018f60619690c5d9214
Chairenerose <76175773+Chairenerose@users.noreply.github.com>
2021-05-02 12:07:23 +0200

    centred video for screens smaller than 650

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 
+
 const hamburger = document.getElementsByClassName('hamburger')[0]
 const navbarlinks = document.getElementsByClassName('nav-links')[0]
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener('click', (event) => {
+    event.preventDefault();
     navbarlinks.classList.toggle('active')
 })
 
@@ -10,6 +12,9 @@ hamburger.addEventListener('click', () => {
 const statementbutton = document.getElementsByClassName('commitmentbutton')[0]
 const statement = document.getElementsByClassName('commitment')[0]
 
-statementbutton.addEventListener('click', () => {
+statementbutton.addEventListener('click', (event) => {
+    event.preventDefault();
     statement.classList.toggle('active')
 })
+
+
